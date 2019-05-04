9269588ba1c4fec22b43386476e005bd0bd787cc
Tim Bedford <tmbdfd@gmail.com>
2019-05-04 16:28:25 -0400

    Append Lab 2 client for Optional Exercise 1
    Calculate minimum ping, maximum ping, average ping, and pack loss rate

--- Lab2/UDPPingerClient.py
+++ Lab2/UDPPingerClient.py
@@ -3,6 +3,7 @@ Lab 1: UDP Pinger Lab
 
 This client sends ten pings to the included server.
 The Round Trip Time is calculated for each ping.
+Minimum ping, maximum ping, average ping, and packet loss rate are also calculated.
 """
 
 from socket import *
@@ -19,6 +20,7 @@ if __name__ == "__main__":
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.settimeout(1.0)
 
+    allRTTs = []
     for i in range(1, 11):
         originalTime = time.time()
         message = "Ping {} {}".format(i, originalTime)
@@ -26,8 +28,15 @@ if __name__ == "__main__":
         try:
             modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
             print(modifiedMessage.decode())
-            print("Round Trip Time (RTT): {}".format(time.time() - originalTime))
+            roundTripTime = time.time() - originalTime
+            print("Round Trip Time (RTT): {}".format(roundTripTime))
+            allRTTs.append(roundTripTime)
         except timeout:
             print("Request timed out")
+
+    print("Minimum ping: {}".format(min(allRTTs)))
+    print("Maximum ping: {}".format(max(allRTTs)))
+    print("Average ping: {}".format(sum(allRTTs)/len(allRTTs)))
+    print("Packet loss rate: {}%".format(len(allRTTs)*10))
 
     clientSocket.close()
