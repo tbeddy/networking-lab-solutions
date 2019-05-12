13aa2ec9eb3c4e4524b1eb80ed7d895fda749481
Tim Bedford <tmbdfd@gmail.com>
2019-05-12 16:20:42 -0400

    Add basic version of Lab 3

---
+++ Lab3/SMTPClient.py
@@ -0,0 +1,66 @@
+from socket import *
+
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = 'localhost' # For basic testing purposes
+
+# Create socket called clientSocket and establish a TCP connection with mailserver
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((mailserver, 25))
+
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '220':
+    print('220 reply not received from server.')
+
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send MAIL FROM command and print server response.
+mailfromCommand = 'MAIL FROM: {}\r\n'.format('foo@bar.com')
+clientSocket.send(mailfromCommand.encode())
+recv2 = clientSocket.recv(1024).decode()
+print(recv2)
+if recv2[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send RCPT TO command and print server response.
+rcptoCommand = 'RCPT TO: <{}>\r\n'.format('bar@foo.com')
+clientSocket.send(rcptoCommand.encode())
+recv3 = clientSocket.recv(1024).decode()
+print(recv3)
+if recv3[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send DATA command and print server response.
+dataCommand = 'DATA\r\n'
+clientSocket.send(dataCommand.encode())
+recv4 = clientSocket.recv(1024).decode()
+print(recv4)
+if recv4[:3] != '354':
+    print('354 reply not received from server.')
+
+# Send message data.
+clientSocket.send(msg.encode())
+
+# Message ends with a single period.
+clientSocket.send(endmsg.encode())
+recv5 = clientSocket.recv(1024).decode()
+print(recv5)
+if recv5[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send QUIT command and get server response.
+quitCommand = 'QUIT\r\n'
+clientSocket.send(quitCommand.encode())
+recv6 = clientSocket.recv(1024).decode()
+print(recv6)
+if recv6[:3] != '221':
+    print('221 reply not received from server.')
